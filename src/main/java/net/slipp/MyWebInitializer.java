45a9f59371cb48669aae033e44495b97f844d871
변재욱 <>
2019-03-14 00:17:50 +0900

    아래 커밋 추가 구현

--- src/main/java/net/slipp/MyWebInitializer.java
+++ src/main/java/net/slipp/MyWebInitializer.java
@@ -1,5 +1,13 @@
 package net.slipp;
 
-public class MyWebInitializer {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class MyWebInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(MySlippApplication.class);
+	}
 
 }
